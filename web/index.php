1b969852493a3b9f495c7a6a938b3e0c2be6e7e5
amukancabbage <amukancabbage@gmail.com>
2020-02-25 22:44:11 +0800

    new: cek level JS/PHP

--- web/index.php
+++ web/index.php
@@ -1,3 +1,27 @@
+<script type="text/javascript">
+function getCookie(cname){
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var jwt = getCookie("jwt");
+if(jwt==""){
+  window.location.replace("pages/account/login.php");
+}
+</script>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include "includes/lib.php"; ?>
