74d40651a5dd985854aeedd2c52240677abc31fb
Cabbage <cumie.banzhar@gmail.com>
2020-06-12 21:28:33 +0800

    user lain username wkwkw

--- web/includes/nav.php
+++ web/includes/nav.php
@@ -8,17 +8,18 @@ $jsonDataEncoded = json_encode($jsonData);
 $url = "https://pmii.banjar.teknobara.co.id/api/pengguna/validate-token.php";
 $result_JSON = request_api($url,$jsonDataEncoded);
 $id = $result_JSON["data"]["id"];
+$user = $result_JSON["data"]["user"];
 
 $level = $result_JSON["data"]["level"];
 if($level==1){
-  $_SESSION["username"] = $result_JSON["data"]["username"];
+  $_SESSION["username"] = $result_JSON["data"]["user"];
   include("sidebar_admin.php");
 }else if($level==3){
   include("sidebar_member.php");
-  $_SESSION["username"] = $result_JSON["data"]["username"];
+  $_SESSION["username"] = $result_JSON["data"]["user"];
 }else if($level==4){
   include("sidebar_guest.php");
-  $_SESSION["username"] = $result_JSON["data"]["username"];
+  $_SESSION["username"] = $result_JSON["data"]["user"];
 }else{
   echo  "<meta http-equiv='refresh' content='1; url=login.php'>  ";
 }
